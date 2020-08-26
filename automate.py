1f87f118ef205bd4cd54de7e6226774c61e18b59
wikyprash <wikyprash@gmail.com>
2020-08-26 19:43:04 +0530

    silly mistake

--- automate.py
+++ automate.py
@@ -188,18 +188,18 @@ class Automate:
             return e
 
     def start(self):
-        # options = Options()
-        # options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        # options.add_argument("--headless")
-        # options.add_argument("--disable-dev-shm-usage")
-        # options.add_argument("--no-sandbox")
-        # driver = webdriver.Chrome(executable_path=os.environ.get(
-        #     "CHROMEDRIVER_PATH"), options=options)
-
         options = Options()
-        options.headless = True
-        driver = webdriver.Chrome(
-            executable_path='res\\chromedriver.exe', options=options)
+        options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        options.add_argument("--headless")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--no-sandbox")
+        driver = webdriver.Chrome(executable_path=os.environ.get(
+            "CHROMEDRIVER_PATH"), options=options)
+
+        # options = Options()
+        # options.headless = True
+        # driver = webdriver.Chrome(
+        #     executable_path='res\\chromedriver.exe', options=options)
 
         urls = Automate.getUrls()
         data = self.getData(driver, urls)
